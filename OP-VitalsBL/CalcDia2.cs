3116ec3e7fadc29160fe3cee525c20c925f641c9
Maiken Guldberg <maiken.g@fibermail.dk>
2017-11-14 11:43:00 +0100

    Tilføjet beregning af sys, dia, mean og puls

---
+++ OP-VitalsBL/CalcDia2.cs
@@ -0,0 +1,51 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using DTO;
+using Interfaces;
+
+namespace OP_VitalsBL
+{
+    class CalcDia2 : ICalcDia
+    {
+        private List<double> analyselist;
+        private List<double> MinList;
+        private double threshold;
+
+        public CalcDia2()
+        {
+            analyselist = new List<double>();
+            MinList = new List<double>();
+            threshold = 80; //Skal måske ændres
+        }
+        public void CalculateDia(double value, BloodpreasureDTO bloodpreasure)
+        {
+            if (analyselist.Count < 3000)
+            {
+                if (value < threshold)
+                {
+                   MinList.Add(value);
+                   analyselist.Add(value);
+                }
+                else
+                {
+                    analyselist.Add(value);
+                    if (MinList.Count > 0)
+                    {
+                        bloodpreasure.Diastole = MinList.Min();
+                        MinList.Clear();
+                    }
+                }
+                analyselist.Add(value);
+            }
+            if (analyselist.Count == 3000)
+            {
+                analyselist.Clear();
+            }
+
+
+        }
+    }
+}
---
+++ OP-VitalsBL/Puls.cs
@@ -0,0 +1,56 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using DTO;
+
+namespace OP_VitalsBL
+{
+    class Puls
+    {
+        private List<double> analyselist;
+        private List<double> pulsList;
+        private int Counthighs;
+        private double threashold;
+
+        public Puls()
+        {
+            analyselist = new List<double>();
+
+            Counthighs = 0;
+        }
+
+        public void CalculatePuls(double value, BloodpreasureDTO bloodpreasure)
+        {
+            if (analyselist.Count == 0)
+            {
+                threashold = bloodpreasure.Systole * 0.80;
+            }
+            if (analyselist.Count < 9000)
+            {
+                if (value > threashold)
+                {
+                    pulsList.Add(value);
+                    analyselist.Add(value);
+                }
+                else
+                {
+                    analyselist.Add(value);
+                    if (pulsList.Count > 0)
+                    {
+                        Counthighs++;
+                        pulsList.Clear();
+                    }
+                }
+                
+            }
+            if (analyselist.Count == 9000)
+            {
+                bloodpreasure.Puls = (Counthighs-1) * (9/60);
+                analyselist.Clear();
+                Counthighs = 0;
+            }
+        }
+    }
+}
