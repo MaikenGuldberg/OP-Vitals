fda5df1b2a955baeec1c968c6b5ba7317c8a9734
Maiken Guldberg <maiken.g@fibermail.dk>
2017-11-15 14:55:00 +0100

    Indsat GIF til nulpunktsjustering

--- OP-VitalsPL/UINulpunktsjustering.Designer.cs
+++ OP-VitalsPL/UINulpunktsjustering.Designer.cs
@@ -37,15 +37,15 @@
             // pictureBox1
             // 
             this.pictureBox1.Image = ((System.Drawing.Image)(resources.GetObject("pictureBox1.Image")));
-            this.pictureBox1.Location = new System.Drawing.Point(142, 112);
+            this.pictureBox1.Location = new System.Drawing.Point(61, 26);
             this.pictureBox1.Name = "pictureBox1";
-            this.pictureBox1.Size = new System.Drawing.Size(33, 32);
+            this.pictureBox1.Size = new System.Drawing.Size(660, 345);
             this.pictureBox1.TabIndex = 0;
             this.pictureBox1.TabStop = false;
             // 
             // button1
             // 
-            this.button1.Location = new System.Drawing.Point(113, 228);
+            this.button1.Location = new System.Drawing.Point(333, 401);
             this.button1.Name = "button1";
             this.button1.Size = new System.Drawing.Size(99, 29);
             this.button1.TabIndex = 1;
@@ -57,7 +57,7 @@
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(358, 297);
+            this.ClientSize = new System.Drawing.Size(782, 449);
             this.Controls.Add(this.button1);
             this.Controls.Add(this.pictureBox1);
             this.Name = "UINulpunktsjustering";
