19be9009cc8064d50b801dc446dd87cda13ea856
Maiken Guldberg <maiken.g@fibermail.dk>
2017-12-14 08:43:23 +0100

    Rettelse på UIKodkendKalibrering

--- OP-VitalsPL/UIGodkendKalibrering.designer.cs
+++ OP-VitalsPL/UIGodkendKalibrering.designer.cs
@@ -40,9 +40,9 @@
             this.label1.Font = new System.Drawing.Font("Microsoft Sans Serif", 14.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
             this.label1.Location = new System.Drawing.Point(42, 63);
             this.label1.Name = "label1";
-            this.label1.Size = new System.Drawing.Size(255, 24);
+            this.label1.Size = new System.Drawing.Size(247, 24);
             this.label1.TabIndex = 0;
-            this.label1.Text = "Skal Kalibrering godkendes ?";
+            this.label1.Text = "Skal kalibrering godkendes?";
             // 
             // CalibrationAccept
             // 
